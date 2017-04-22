c4d864594c5077143d11732f5d15a324d976bbf3
Valmar Júnior <valmar.junior1@gmail.com>
2017-04-22 20:56:44 -0300

    Initial commit

---
+++ app/src/main/java/br/vj/ancalagon/redditreader/MainActivity.java
@@ -0,0 +1,11 @@
+package br.vj.ancalagon.redditreader;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
