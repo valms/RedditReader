960003f0e51cf8ae604e74d9bb95d6dd425fcbf4
Valmar Júnior <valmar.junior1@gmail.com>
2017-04-23 23:06:29 -0300

    Configuração do Projeto para utilização do Android Annotations

--- app/src/main/java/br/vj/ancalagon/redditreader/MainActivity.java
+++ app/src/main/java/br/vj/ancalagon/redditreader/MainActivity.java
@@ -1,11 +1,16 @@
 package br.vj.ancalagon.redditreader;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.WindowFeature;
 
-
+@Fullscreen
+@WindowFeature(Window.FEATURE_NO_TITLE)
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
 
 }
